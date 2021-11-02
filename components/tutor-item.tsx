e0972054271ededcdf523fa96d33858a62a36a32
Hoa Doan <hoa.doan@itdragons.com>
2021-11-02 11:14:27 +0700

    Apply template tutor list

--- components/tutor-item.tsx
+++ components/tutor-item.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import Link from 'next/link';
 import { ITutor } from '@model/tutor.model';
+import {Address} from "@components/address";
 
-interface ITutors {
-  data?: ITutor[];
+interface ITutorItemProps {
+  tutor?: ITutor;
 }
 
-export const TutorItem = ({ data }: ITutors) => {
+export const TutorItem = (props: ITutorItemProps) => {
+  const { tutor } = props;
   return (
-    <ul className="list-unstyled mt-2">
-      {data?.map((tutor, index) => (
-        <li className="mb-4" key={index}>
-          <Link href="/tutors/[id]" as={`/tutors/${tutor.id}`} key={index}>
-            <a>
-              <h5 className="mb-2 text-primary">
-                <strong>{tutor.userInfo.user.firstName} {tutor.userInfo.user.lastName}</strong>
-              </h5>
-            </a>
-          </Link>
-        </li>
-      ))}
-      { !data?.length ? <h3 className="text-tutor text-error my-5">No tutor found!</h3> : '' }
-    </ul>
+    <div className="course-block style-two col-lg-3 col-md-6 col-sm-12">
+      <div className="inner-box">
+        <div className="image">
+          <a href="#">
+            <img src={`data:${tutor.userInfo.avatarContentType};base64,${tutor.userInfo.avatar}`} />
+          </a>
+        </div>
+        <div className="lower-content">
+          <h4><a href="#">{tutor.userInfo.user.firstName} {tutor.userInfo.user.lastName}</a></h4>
+          <div className="rating">
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star-o" />
+            <strong>4.9</strong>
+            <i>(70 Review)</i>
+          </div>
+        </div>
+        {/* Overlay Content Box */}
+        <div className="overlay-content-box">
+          <h4>{tutor.userInfo.user.firstName} {tutor.userInfo.user.lastName}</h4>
+          <div className="rating">
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star-o" />
+            <strong>4.9</strong>
+            <i>(70 Review)</i>
+          </div>
+          {tutor && tutor.userInfo.ward ? (
+            <div className="text">
+              <Address ward={tutor.userInfo.ward}/>
+            </div>
+          ) : ''}
+          {tutor.userInfo && tutor.userInfo.note ? (
+            <div className="text">{tutor.userInfo.note}</div>
+          ) : ''}
+        </div>
+        {/* End Overlay Content Box */}
+      </div>
+    </div>
   );
 };
